5e5ea861f192bdc1a121fd348dcc975119351c4f
mbd-s <michaelbsilverberg@gmail.com>
2016-04-19 20:50:28 -0700

    spelling

--- starter-code/app/js/app.js
+++ starter-code/app/js/app.js
@@ -44,7 +44,7 @@ function PlayersController(){
     {name: 'Gilligan', points: 2},
     {name: 'Mary Ann', points: 16},
     {name: 'Ginger', points: 12},
-    {name: 'Thurson', points: 6},
+    {name: 'Thurston', points: 6},
     {name: 'Skipper', points: 9},
     {name: 'The Professor', points: 21}
   ];
